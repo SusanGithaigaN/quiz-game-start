a224d6ff8cc93935426b656d74aa0dacbff85f89
Susan Njeri Githaiga <susangithaiga26@gmail.com>
2023-07-19 09:32:41 +0300

    Completed day 17

--- quiz_brain.py
+++ quiz_brain.py
@@ -1,6 +1,7 @@
 class QuizBrain:
     def __init__(self, q_list):
         self.question_number = 0
+        self.score = 0
         self.question_list = q_list
 
     def still_has_questions(self):
@@ -14,7 +15,11 @@ class QuizBrain:
 
     def check_answer(self, user_answer, correct_answer):
         if user_answer.lower() == correct_answer.lower():
+            self.score +=1
             print("You got it right!")
         else:
             print("That's wrong meathead")
         print(f"The answer is: {correct_answer}.")
+        print(f"Your current score is: {self.score}/{self.question_number}")
+        # add a new line
+        print("\n")
