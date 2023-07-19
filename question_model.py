e1637ecbf66b4cc6b5e215765daba04269484fa0
Susan Njeri Githaiga <susangithaiga26@gmail.com>
2023-07-19 12:19:24 +0300

    Added a new database

--- question_model.py
+++ question_model.py
@@ -1,8 +1,8 @@
 class Question:
     def __init__(self, q_text, q_answer):
         # pass
-        self.text = q_text
-        self.answer = q_answer
+        self.question = q_text
+        self.correct_answer = q_answer
 
 
 # new_q = Question("Who are you?", "I don't know, I just got here")
--- quiz_brain.py
+++ quiz_brain.py
@@ -10,8 +10,8 @@ class QuizBrain:
     def next_question(self):
         current_question =self.question_list[self.question_number]
         self.question_number += 1
-        user_answer = input(f"Q{self.question_number}: {current_question.text} (True/False)?: ")
-        self.check_answer(user_answer, current_question.answer)
+        user_answer = input(f"Q{self.question_number}: {current_question.question} (True/False)?: ")
+        self.check_answer(user_answer, current_question.correct_answer)
 
     def check_answer(self, user_answer, correct_answer):
         if user_answer.lower() == correct_answer.lower():
